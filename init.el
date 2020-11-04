8cfc5d945fd2899e95119e8a81589eb8b817647b
Mario Car <Mario.Car@ericsson.com>
2020-11-04 14:08:58 +0100

    Updated scratch message

--- init.el
+++ init.el
@@ -36,7 +36,8 @@
 (csetq scroll-bar-mode nil)
 (csetq truncate-lines t)
 (csetq inhibit-startup-screen t)
-(csetq initial-scratch-message "")
+(setq-default initial-scratch-message
+              (concat ";; Happy hacking " user-login-name "!\n\n"))
 (csetq text-quoting-style 'grave)
 (csetq line-number-display-limit-width 2000000)
 ;;** navigation within buffer
